ada80da1330a29b659b0fa807fd8ef736941b766
Andrey Polkovnichenko <andr3colonel@gmail.com>
2019-03-16 12:41:18 +0200

    Parse imports

---
+++ src/main/java/wasm/format/WasmEnums.java
@@ -0,0 +1,11 @@
+package wasm.format;
+
+public class WasmEnums {
+	public enum WasmExternalKind {
+		EXT_FUNCTION,
+		EXT_TABLE,
+		EXT_MEMORY,
+		EXT_GLOBAL
+	}
+
+}
---
+++ src/main/java/wasm/format/sections/structures/WasmResizableLimits.java
@@ -0,0 +1,39 @@
+package wasm.format.sections.structures;
+
+import java.io.IOException;
+
+import ghidra.app.util.bin.BinaryReader;
+import ghidra.app.util.bin.StructConverter;
+import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.Structure;
+import ghidra.program.model.data.StructureDataType;
+import ghidra.util.exception.DuplicateNameException;
+import wasm.format.Leb128;
+
+public class WasmResizableLimits implements StructConverter {
+
+	byte flags;
+	Leb128 initial;
+	Leb128 maximum;
+	
+	public WasmResizableLimits  (BinaryReader reader) throws IOException {
+		flags = reader.readNextByte();
+		initial = new Leb128(reader);
+		if (flags == 1) {
+			maximum = new Leb128(reader);
+		}
+	}
+
+	@Override
+	public DataType toDataType() throws DuplicateNameException, IOException {
+		Structure structure = new StructureDataType("global", 0);
+		structure.add(BYTE, 1, "flags", null);
+		structure.add(initial.getType(), initial.getSize(), "mutability", null);
+		if (flags == 1) {
+			structure.add(maximum.getType(), maximum.getSize(), "maximum", null);
+		}
+		return structure;
+	}
+
+
+}
