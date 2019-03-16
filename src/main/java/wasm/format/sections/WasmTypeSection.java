4522b92c376dab07289e7401c1698c8dd0a17b88
Andrey Polkovnichenko <andr3colonel@gmail.com>
2019-03-16 13:42:30 +0200

    Add table section

--- src/main/java/wasm/format/sections/WasmTypeSection.java
+++ src/main/java/wasm/format/sections/WasmTypeSection.java
@@ -1,22 +1,38 @@
 package wasm.format.sections;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import ghidra.app.util.bin.BinaryReader;
 import ghidra.app.util.bin.StructConverter;
 import ghidra.program.model.data.DataType;
 import ghidra.program.model.data.Structure;
 import ghidra.util.exception.DuplicateNameException;
+import wasm.format.Leb128;
+import wasm.format.sections.structures.WasmExportEntry;
+import wasm.format.sections.structures.WasmFuncType;
 
 public class WasmTypeSection extends WasmPayload {
 
+	private Leb128 count;
+	private List<WasmFuncType> types = new ArrayList<WasmFuncType>();
+	
 	public WasmTypeSection (BinaryReader reader) throws IOException {
+		count = new Leb128(reader);
+		for (int i =0; i < count.getValue(); ++i) {
+			types.add(new WasmFuncType(reader));
+		}
+		
 	}
 
+
 	@Override
-	public void fillPayloadStruct(Structure structure) {
-		// TODO Auto-generated method stub
-		
+	public void fillPayloadStruct(Structure structure) throws DuplicateNameException, IOException {
+		structure.add(count.getType(), count.getSize(), "count", null);
+		for (int i = 0; i < count.getValue(); ++i) {
+			structure.add(types.get(i).toDataType(), types.get(i).toDataType().getLength(), "type_"+i, null);
+		}		
 	}
 
 	@Override
