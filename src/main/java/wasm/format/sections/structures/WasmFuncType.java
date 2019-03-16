9aa892008d199dec31a67620bbdbcec93258c85b
Andrey Polkovnichenko <andr3colonel@gmail.com>
2019-03-16 13:40:52 +0200

    Add functions section

---
+++ src/main/java/wasm/format/sections/structures/WasmFuncType.java
@@ -0,0 +1,49 @@
+package wasm.format.sections.structures;
+
+import java.io.IOException;
+
+import ghidra.app.util.bin.BinaryReader;
+import ghidra.app.util.bin.StructConverter;
+import ghidra.program.model.data.ArrayDataType;
+import ghidra.program.model.data.DataType;
+import ghidra.program.model.data.Structure;
+import ghidra.program.model.data.StructureDataType;
+import ghidra.util.exception.DuplicateNameException;
+import wasm.format.Leb128;
+
+public class WasmFuncType implements StructConverter {
+
+	byte form;
+	Leb128 param_count;
+	byte[] param_types;
+	Leb128 return_count;
+	byte[] return_types;
+	
+	public WasmFuncType (BinaryReader reader) throws IOException {
+		form = reader.readNextByte();
+		param_count = new Leb128(reader);
+		if (param_count.getValue() > 0) {
+			param_types = reader.readNextByteArray(param_count.getValue());			
+		}
+		return_count = new Leb128(reader);
+		if (return_count.getValue() > 0) {
+			return_types = reader.readNextByteArray(return_count.getValue());			
+		}
+	}
+
+	@Override
+	public DataType toDataType() throws DuplicateNameException, IOException {
+		Structure structure = new StructureDataType("funct_type", 0);
+		structure.add(BYTE, 1, "form", null);
+		structure.add(param_count.getType(), param_count.getSize(), "param_count", null);
+		if (param_count.getValue() > 0) {
+			structure.add(new ArrayDataType(BYTE, param_count.getValue(), 1), "param_types", null);
+		}
+		structure.add(return_count.getType(), return_count.getSize(), "return_count", null);
+		if (return_count.getValue() > 0) {
+			structure.add(new ArrayDataType(BYTE, return_count.getValue(), 1), "return_types", null);
+		}
+		return structure;
+	}
+
+}
